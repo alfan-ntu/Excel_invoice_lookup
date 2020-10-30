9b7460da5c5347640c845703d08ed763186ec4d3
alfan-ntu <maoyi_fan@yahoo.com.tw>
2020-10-30 23:05:12 +0800

    Rearrange utility functions by moving some widgets to utility.py

--- utility.py
+++ utility.py
@@ -6,7 +6,9 @@
 # Programmer: alfan-ntu
 # Date: 2020/10/4
 #
+import xlrd
 import logging
+import constant
 
 
 def initialization():
@@ -14,7 +16,9 @@ def initialization():
     logging.basicConfig(filename="./log/excel_lookup.log", filemode='w', format='%(asctime)s %(levelname)s:%(message)s',
                         datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
 
-
+#
+# convert comma separated currency annotation to its floating number
+#
 def comma_separated_amount_to_float(amt_str):
     amt_no_comma = ""
     for i in range(len(amt_str)):
@@ -23,3 +27,77 @@ def comma_separated_amount_to_float(amt_str):
     amount = float(amt_no_comma)
     return amount
 
+
+#
+# Looking for exchange rate from the remark column. SUPPOSE all currency exchange rates
+# lead with "匯率"
+#
+def find_currency_exchange_rate(sourceRow):
+    ex_rate = 1.0
+    remark = sourceRow[constant.COL_INVOICE_REMARK].value
+    idxEx = remark.find(constant.EXCHANGE_RATE_LEADING_CHRS)
+    idxUsdAmt = remark.find(constant.USD_AMOUNT_CHRS)
+    # it is an transaction of USD if both exchange_rate and USD sales amount found
+    if idxEx >= 0 and idxUsdAmt >=0:
+        ex_rate = float(remark[(idxEx+4):(idxUsdAmt-2)])
+
+    return ex_rate
+
+
+#
+# Claim it is a USD transaction if "註記欄" includes both "匯率" and "美金未稅"
+#
+def is_source_a_usd_transaction(source_row):
+    remark = source_row[constant.COL_INVOICE_REMARK].value
+    idx_ex = remark.find(constant.EXCHANGE_RATE_LEADING_CHRS)
+    idx_usd_amt = remark.find(constant.USD_AMOUNT_CHRS)
+    if idx_ex >= 0 and idx_usd_amt >= 0:
+        return True
+    else:
+        return False
+
+
+#
+# Claim the target transaction is a USD transaction if the column 'Currency Rate' > 1
+#
+def is_target_a_usd_transaction(targetRow):
+    # pdb.set_trace()
+    currency_exchange_rate = targetRow[constant.COL_GL_EXCHANGE_RATE].value
+    ex_rate = float(currency_exchange_rate)
+    if ex_rate > 1.0:
+        return True
+    else:
+        return False
+
+
+#
+# Target records need to be further processed include
+#   1. Account Description includes constant.TARGET_ACCOUNT_IN_GL (Accounts Receivable, actually)
+#   2. Amount > 0; Amount <= 0 means Account Receivables received
+#   3. Voucher Date is in the specified accounting period
+#   ToDo's : needs to implement specified accounting period
+#
+def is_target_account_receivable(targetRow):
+    targetCell = targetRow[constant.COL_GL_ACCOUNT_DESCRIPTION]
+    account = targetCell.value
+    idxAccount = account.find(constant.TARGET_ACCOUNT_IN_GL)
+    amount = targetRow[constant.COL_GL_AMOUNT].value
+
+    if idxAccount < 0:
+        # print("Not an Account Receivable")
+        return False
+    if targetRow[constant.COL_GL_AMOUNT].ctype == xlrd.XL_CELL_EMPTY or \
+            targetRow[constant.COL_GL_AMOUNT].ctype == xlrd.XL_CELL_BLANK:
+        # print("Encountering Empty Amount Cell")
+        return False
+    # Looks like IFS output string of length 1, for which cell is not empty,
+    # not blank, and float() fails to convert it to a floating number
+    if targetRow[constant.COL_GL_AMOUNT].ctype == xlrd.XL_CELL_TEXT:
+        if len(amount) == 1:
+            # print("Not a valid amount")
+            return False
+    # amount of a record is negative when the "Account Receivable" is received
+    if float(amount) < 0.0:
+        # print("Account Receivable received")
+        return False
+    return True
--- xlsrw_oop.py
+++ xlsrw_oop.py
@@ -4,7 +4,10 @@
 # Coder: alfan-ntu
 # Created Date: 2020/10/2
 # Revision:
-#   1. 2020/10/2: 1st creation
+#   1. 2020/10/2: v. 0.1 1st creation
+#   2. 2020/10/30: v. 0.2
+#           - rearrange utility functions by moving some widgets to utility.py
+#           - modify Invoice Details to include a matched mark
 #
 # ToDo's:
 #   1. Add an argument parser to accept source invoice records, target general ledger file, specified invoicing date
@@ -15,7 +18,8 @@
 #   2. xlwt can generate spreadsheet file compatible with .xls (MS Excel 97/2000/XP/200) BUT NOT .xlsx
 #   3. openpyxl can read/write Excel 2010 xlsx/xlsm/xltx/xltm files, BUT NOT .xls
 #   Both xlrd/xlwt/xlutils and openpyxl packages are required to process the input
-#   invoice files and general ledger files.
+#   invoice files and general ledger files since Invoice Details is .xls while General Ledger
+#   is .xlsx. Pandas might be a flexible and more versatile alternative.
 #
 import sys
 import xlsxwriter
@@ -56,7 +60,7 @@ def match_row(sourceRow, targetWs):
     # Continue traversing target worksheet only if the invoice in the sourceRow is a valid one
     # print(sourceRow[constant.COL_INVOICE_NO].value, " is valid")
     # for jt in (1, targetWs.nrows):
-    ex_rate = find_currency_exchange_rate(sourceRow)
+    ex_rate = utility.find_currency_exchange_rate(sourceRow)
     if ex_rate == 1.0:
         pass
     else:
@@ -64,79 +68,6 @@ def match_row(sourceRow, targetWs):
     return True, 20
 
 
-#
-# Claim it is a USD transaction if "註記欄" includes both "匯率" and "美金未稅"
-#
-def is_source_a_usd_transaction(source_row):
-    remark = source_row[constant.COL_INVOICE_REMARK].value
-    idx_ex = remark.find(constant.EXCHANGE_RATE_LEADING_CHRS)
-    idx_usd_amt = remark.find(constant.USD_AMOUNT_CHRS)
-    if idx_ex >= 0 and idx_usd_amt >= 0:
-        return True
-    else:
-        return False
-
-
-#
-# Looking for exchange rate from the remark column. SUPPOSE all currency exchange rates
-# lead with "匯率"
-#
-def find_currency_exchange_rate(sourceRow):
-    ex_rate = 1.0
-    remark = sourceRow[constant.COL_INVOICE_REMARK].value
-    idxEx = remark.find(constant.EXCHANGE_RATE_LEADING_CHRS)
-    idxUsdAmt = remark.find(constant.USD_AMOUNT_CHRS)
-    # it is an transaction of USD if both exchange_rate and USD sales amount found
-    if idxEx >= 0 and idxUsdAmt >=0:
-        ex_rate = float(remark[(idxEx+4):(idxUsdAmt-2)])
-
-    return ex_rate
-
-
-#
-# Claim the target transaction is a USD transaction if the column 'Currency Rate' > 1
-#
-def is_target_a_usd_transaction(targetRow):
-    # pdb.set_trace()
-    currency_exchange_rate = targetRow[constant.COL_GL_EXCHANGE_RATE].value
-    ex_rate = float(currency_exchange_rate)
-    if ex_rate > 1.0:
-        return True
-    else:
-        return False
-
-
-#
-# Target records need to be further processed include
-#   1. Account Description includes constant.TARGET_ACCOUNT_IN_GL (Accounts Receivable, actually)
-#   2. Amount > 0; Amount <= 0 means Account Receivables received
-#   3. Voucher Date is in the specified accounting period
-#   ToDo's : needs to implement specified accounting period
-#
-def is_target_account_receivable(targetRow):
-    targetCell = targetRow[constant.COL_GL_ACCOUNT_DESCRIPTION]
-    account = targetCell.value
-    idxAccount = account.find(constant.TARGET_ACCOUNT_IN_GL)
-    amount = targetRow[constant.COL_GL_AMOUNT].value
-
-    if idxAccount < 0:
-        # print("Not an Account Receivable")
-        return False
-    if targetRow[constant.COL_GL_AMOUNT].ctype == xlrd.XL_CELL_EMPTY or \
-            targetRow[constant.COL_GL_AMOUNT].ctype == xlrd.XL_CELL_BLANK:
-        # print("Encountering Empty Amount Cell")
-        return False
-    # Looks like IFS output string of length 1, for which cell is not empty,
-    # not blank, and float() fails to convert it to a floating number
-    if targetRow[constant.COL_GL_AMOUNT].ctype == xlrd.XL_CELL_TEXT:
-        if len(amount) == 1:
-            # print("Not a valid amount")
-            return False
-    # amount of a record is negative when the "Account Receivable" is received
-    if float(amount) < 0.0:
-        # print("Account Receivable received")
-        return False
-    return True
 
 
 def main(argv):
@@ -152,7 +83,6 @@ def main(argv):
     sourceWs = sourceWb.sheet_by_name(sheetName)
     sourceWb_temp = xlutils_copy(sourceWb)
     sourceWs_temp = sourceWb_temp.get_sheet(0)
-    print(sourceWb_temp)
     #
     # Open target general ledger Excel file
     # generalLedger = "./Voucher_Row_Analysis_20200930.xlsx"
@@ -179,9 +109,9 @@ def main(argv):
         invoice_amount_nt = utility.comma_separated_amount_to_float(amount_nt_str)
         # determine if it is a USD transaction, extract the exchange rate if
         # it is a USD transaction
-        if is_source_a_usd_transaction(sourceWs.row(js)):
+        if utility.is_source_a_usd_transaction(sourceWs.row(js)):
             function_currency = constant.FUNCTION_CURRENCY_USD
-            exchange_rate = find_currency_exchange_rate(sourceWs.row(js))
+            exchange_rate = utility.find_currency_exchange_rate(sourceWs.row(js))
         else:
             function_currency = constant.FUNCTION_CURRENCY_NTD
             exchange_rate = 1.00
@@ -202,7 +132,7 @@ def main(argv):
         # source transaction
         match_found = False
         for jt in range(1, targetWs.nrows):
-            if not is_target_account_receivable(targetWs.row(jt)):
+            if not utility.is_target_account_receivable(targetWs.row(jt)):
                 continue
             invoice_number = targetWs.cell_value(jt, constant.COL_GL_INVOICE_NO)
             # ToDo's : the type of this voucher number is read as a floating number, which is different for
@@ -211,7 +141,7 @@ def main(argv):
             buyer_name = targetWs.cell_value(jt, constant.COL_GL_TEXT)
             invoice_date = targetWs.cell_value(jt, constant.COL_GL_INVOICE_DATE)
             invoice_amount_nt = targetWs.cell_value(jt, constant.COL_GL_AMOUNT)
-            if is_target_a_usd_transaction(targetWs.row(jt)):
+            if utility.is_target_a_usd_transaction(targetWs.row(jt)):
                 function_currency = constant.FUNCTION_CURRENCY_USD
                 exchange_rate = targetWs.cell_value(jt, constant.COL_GL_EXCHANGE_RATE)
                 invoice_amount_us = invoice_amount_nt / exchange_rate
